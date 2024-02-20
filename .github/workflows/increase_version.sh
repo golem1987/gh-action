927b715d53baba6cd2c922f514c629eb2cce72e8
golem1987 <12938890+golem1987@users.noreply.github.com>
2024-02-20 14:34:51 +0000

    Create increase_version.sh

---
+++ .github/workflows/increase_version.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+set -exu
+
+DEVELOP_VERSION=$1
+BRANCH_VERSION=$2
+
+function semver_or_die() {
+    local version
+    version="$1"
+    if [[ ! ${version} =~ ^[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
+        printf >&2 'Error : %s is not a valid semver.\n' $version
+        exit 1
+    fi
+}
+
+semver_or_die $DEVELOP_VERSION
+semver_or_die $BRANCH_VERSION
+
+
+if [[ $# -eq 0 || -z $1 || -z $2 ]]; then
+    printf >&2 'Error : missing required arguments.\n\n'
+    exit 1
+fi
+
+IFS='.' read -r MAJOR MINOR PATCH <<< "$DEVELOP_VERSION"
+MAJOR_DEVELOP=${MAJOR}
+MINOR_DEVELOP=${MINOR}
+PATCH_DEVELOP=${PATCH}
+DEV_VERSION="$MAJOR_DEVELOP.$MINOR_DEVELOP.$PATCH_DEVELOP" 
+
+IFS='.' read -r MAJOR MINOR PATCH <<< "$BRANCH_VERSION"
+MAJOR_BRANCH=${MAJOR}
+MINOR_BRANCH=${MINOR}
+PATCH_BRANCH=${PATCH}
+BRANCH_VERSION="$MAJOR_BRANCH.$MINOR_BRANCH.$PATCH_BRANCH" 
+
+
+if [[ "$MAJOR_DEVELOP" == "$MAJOR_BRANCH" && "$MINOR_DEVELOP" == "$MINOR_BRANCH" ]]; then
+    PATCH_DEVELOP=$((PATCH_DEVELOP+1))
+    NEW_VERSION="$MAJOR_DEVELOP.$MINOR_DEVELOP.$PATCH_DEVELOP" 
+    echo "::set-output name=stdout::New chart version $NEW_VERSION"
+else
+    NEW_VERSION=$BRANCH_VERSION
+    echo "::set-output name=stdout::New chart version $NEW_VERSION"
+fi
+
+echo $value
